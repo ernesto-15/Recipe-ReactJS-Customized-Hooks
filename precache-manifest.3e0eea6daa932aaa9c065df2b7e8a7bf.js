a1da6b2c52cd85d049de951b182cff1252c2a282
Ernesto Angulo <ernestoangulojimenez@gmail.com>
2020-04-29 15:57:03 -0500

    Updates

---
+++ precache-manifest.3e0eea6daa932aaa9c065df2b7e8a7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0facef8a8a3f1c89283d29a3a3a9bb50",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "9df24d971275bf1a11f4",
+    "url": "/repository-name/static/css/main.3cb48c60.chunk.css"
+  },
+  {
+    "revision": "dec5d2ce4ade34771158",
+    "url": "/repository-name/static/js/2.35d6c590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repository-name/static/js/2.35d6c590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df24d971275bf1a11f4",
+    "url": "/repository-name/static/js/main.53241a19.chunk.js"
+  },
+  {
+    "revision": "785dd9f5df944663f847",
+    "url": "/repository-name/static/js/runtime-main.8fc6ee88.js"
+  }
+]);
